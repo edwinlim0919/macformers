738e95f76b6086299ed818fa7a88927685bbe24f
Edwin Lim <edwinlim0919@gmail.com>
2024-08-13 14:39:11 -0700

    initial repo structure

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Setup and instructions inspired from the following blog post by Lars Gebraad
+# - https://larsgeb.github.io/2022/04/20/m1-gpu.html
+
+# NOTE: Need Xcode installed for Metal SDK dependencies
+brew install llvm
+brew install libomp
+
+#
